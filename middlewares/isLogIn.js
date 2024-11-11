9a44819647671c8cf1d069e6b8aabb403c7ee087
Amir Dhahri <amirdhahri01@gmail.com>
2024-11-11 17:24:50 +0100

    An update

--- middlewares/isLogIn.js
+++ middlewares/isLogIn.js
@@ -1,13 +1,13 @@
 import Admin from "../models/Staff/Admin.js";
 import verifyToken from "../utils/verifyToken.js";
 
-const isLogin = (req, res, next) => {
+const isLogin = async (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1];
   const verifiedToken = verifyToken(token);
   if (!verifiedToken) {
     next(new Error("Token expired/invalid"));
   }
-  const user = Admin.findById(verifiedToken.id).select("name email role");
+  const user = await Admin.findById(verifiedToken.id).select("name email role");
   req.userAuth = user;
   next();
 };
